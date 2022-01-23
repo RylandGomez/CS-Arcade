09460a7c948a32e513f5973be47350faf823f34a
RylandGomez <rylandalexandergomez@gmail.com>
2022-01-22 21:25:56 -0600

    Restructured Organization, Third Task
    Added subgroups of tasks within Intro, and added the third task.

---
+++ Intro/The_Journey_Begins/03_checkPalindrome.py
@@ -0,0 +1,30 @@
+def solution(inputString):
+    '''
+    PROMPT
+    -------------------------------------------------------------------
+    Given the string, check if it is a palindrome.
+    A palindrome is a string that doesn't changed when reversed (it
+    reads the same backward and forward).
+    -------------------------------------------------------------------
+    '''
+    return inputString == inputString[::-1]
+    # Slice of [::-1] implies a start to finish slice of -1 steps 
+    # (going backwards through the string). This is the reversed string.
+
+def alternate(inputString):
+    '''
+    -------------------------------------------------------------------
+    An alternate method is to iterate through the string, comparing the
+    first character to the last one, then the second to the character
+    before the last one, and so on. This is demonstrated below.
+    -------------------------------------------------------------------
+    '''
+    for i in range(len(inputString) // 2):
+        if inputString[i] != inputString[-1 - i]:
+            return False
+    return True
+    # len(inputString) // 2 is used to iterate over the first half.
+    # The inputString[-1 - i] uses this iteration to reference the back
+    # half of the string simultaneously. If they do not match, the
+    # algorithm returns False, breaking the loop. If the loop finishes
+    # without breaking, the algorithm returns True.
